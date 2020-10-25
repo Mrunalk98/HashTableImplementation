67492998991f516d7565b0cceeb08fc57202a60d
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-25 20:19:43 +0530

    [mrunal] Add. Created MyMapNode class with Key Value Pair and linked list of MyMapNode

--- HashTableImplementation/Program.cs
+++ HashTableImplementation/Program.cs
@@ -7,6 +7,16 @@ namespace HashTableImplementation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hash Table Program!");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string result5 = hash.Get("5");
+            Console.WriteLine("Value at 5th index : " + result5);
         }
     }
 }
